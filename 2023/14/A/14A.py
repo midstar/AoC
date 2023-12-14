c179ede3d1634f606c4aefc5f54cd3ef5314e854
Joel Midstjärna <joel.midstjarna@gmail.com>
2023-12-14 21:51:03 +0100

    Puzzle 14 solved

--- 2023/14/A/14A.py
+++ 2023/14/A/14A.py
@@ -18,13 +18,6 @@ def tilt(pattern):
                     changed = True
     return pattern
 
-def score(pattern):
-    score = 0
-    for row in range(0, len(pattern)):
-        score_base = len(pattern) - row
-        score += pattern[row].count('O') * score_base
-    return score
-
 def transpose(pattern):
     new = []
     for col in range(0, len(pattern[0])):
@@ -38,6 +31,13 @@ def print_pattern(pattern):
     for line in pattern:
         print(''.join(line))
 
+def score(pattern):
+    score = 0
+    for row in range(0, len(pattern)):
+        score_base = len(pattern) - row
+        score += pattern[row].count('O') * score_base
+    return score
+
 
 def solve(input):
     lines = input.splitlines()
@@ -45,44 +45,21 @@ def solve(input):
     for line in lines:
         pattern.append([*line])
 
-    #print_pattern(pattern)
-    #print()
+    print_pattern(pattern)
+    print()
+    pattern = transpose(pattern)
+    print_pattern(pattern)
+    print()
+    pattern = tilt(pattern)
+    print_pattern(pattern)
+    print()
+    pattern = transpose(pattern)
+    pattern = transpose(pattern)
+    pattern = transpose(pattern)
+    print_pattern(pattern)
+    print()
 
-    scores = []
-    for i in range(0, 500): #1000000000):
-        for i in range(0, 4):
-            pattern = transpose(pattern)
-            pattern = tilt(pattern)
-        scores.append(score(pattern))
-
-    # Find repeating pattern
-    start_index = 100
-    repeating = scores[start_index:start_index + 100] # Try some
-    repeating_indexes = []
-    for i in range(1, len(scores) - len(repeating)):
-        repeating_index = i
-        for j in range(0, len(repeating)):
-            if scores[i+j] != repeating[j]:
-                repeating_index = -1
-                break
-        if repeating_index != -1:
-            repeating_indexes.append(repeating_index)
-
-    print(repeating_indexes)
-
-    # Validate repeating pattern
-    offset = repeating_indexes[1] - repeating_indexes[0]
-    for i in range(0, len(repeating_indexes) - 1):
-        if repeating_indexes[i+1] - repeating_indexes[i] != offset:
-            print("No pattern found!")
-            exit(1)
-    
-    repeating = repeating[:offset]
-    print('Offset', offset)
-    print('Sequence', repeating)
-
-    pattern_offset = (1000000000 - start_index) % offset
-    result = repeating[pattern_offset - 1]
+    result = score(pattern)
 
     print('Result: %s' % result)
 
