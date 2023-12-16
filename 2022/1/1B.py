02f84d9675030f4cecc8460af227430ec32b1a28
Joel Midstjärna <joel.midstjarna@gmail.com>
2023-12-17 00:03:51 +0100

    2022 Puzzle 1 solved

---
+++ 2022/1/1B.py
@@ -0,0 +1,26 @@
+import argparse, time
+
+def solve(input):
+    elf_calories = []
+    for elf in input.split('\n\n'):
+        tot_calories = 0
+        for calories in elf.splitlines():
+            tot_calories += int(calories)
+        elf_calories.append(tot_calories)
+    elf_calories.sort()
+    return sum(elf_calories[-3:])
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='Advent Of Code Solver %s' % __file__)
+    parser.add_argument('input_filename', help='Input', 
+                        nargs='?', default='input.txt')
+    args = vars(parser.parse_args())
+
+    with open(args['input_filename'],'r') as f:
+        start_time = time.time()
+        result = solve(f.read())
+        print('Result: %s  (excution time %0.4f s)' % (result, time.time() - start_time))
+
+if __name__ == '__main__':
+    main()    
