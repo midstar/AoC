d224d4ee813feb7f85c9e5f3b71c26cca52fb791
Joel Midstjärna <joel.midstjarna@gmail.com>
2023-12-16 07:10:38 +0100

    Puzzle 16 solved

---
+++ 2023/16/A/16A.py
@@ -0,0 +1,100 @@
+import argparse, sys
+
+
+
+def traverse(matrix, row, col, dir):
+    if row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[row]):
+        return
+    p = matrix[row][col]
+    if p[dir]:
+        return # Already been here from this direction
+    p[dir] = True
+    if p['obj'] == '.':
+        if dir == 'up':
+            traverse(matrix, row - 1, col, 'up')
+        elif dir == 'down':
+            traverse(matrix, row + 1, col, 'down')
+        elif dir == 'right':
+            traverse(matrix, row, col + 1, 'right')
+        elif dir == 'left':
+            traverse(matrix, row, col - 1, 'left')
+    elif p['obj'] == '-':
+        if dir == 'up' or  dir == 'down':
+            traverse(matrix, row, col + 1, 'right')
+            traverse(matrix, row, col - 1, 'left')
+        elif dir == 'right':
+            traverse(matrix, row, col + 1, 'right')
+        elif dir == 'left':
+            traverse(matrix, row, col - 1, 'left')
+    elif p['obj'] == '|':
+        if dir == 'up':
+            traverse(matrix, row - 1, col, 'up')
+        elif dir == 'down':
+            traverse(matrix, row + 1, col, 'down')
+        if dir == 'right' or  dir == 'left':
+            traverse(matrix, row - 1, col, 'up')
+            traverse(matrix, row + 1, col, 'down')
+    elif p['obj'] == '/':
+        if dir == 'up':
+            traverse(matrix, row, col + 1, 'right')
+        elif dir == 'down':
+            traverse(matrix, row, col - 1, 'left')
+        elif dir == 'right':
+            traverse(matrix, row - 1, col, 'up')
+        elif dir == 'left':
+            traverse(matrix, row + 1, col, 'down')
+    elif p['obj'] == '\\':
+        print(row, col, 'match', dir)
+        if dir == 'up':
+            traverse(matrix, row, col - 1, 'left')
+        elif dir == 'down':
+            traverse(matrix, row, col + 1, 'right')
+        elif dir == 'right':
+            traverse(matrix, row + 1, col, 'down')
+        elif dir == 'left':
+            traverse(matrix, row - 1, col, 'up')
+
+
+
+def solve(input):
+    lines = input.splitlines()
+    matrix = []
+    for row in lines:
+        mrow = []
+        for col in row:
+            mrow.append({
+                'obj' : col,
+                'up' : False, 
+                'down' : False, 
+                'right' : False, 
+                'left' : False
+            })
+        matrix.append(mrow)
+
+    sys.setrecursionlimit(100000)
+    traverse(matrix, 0, 0, 'right')
+
+    result = 0
+    for row in matrix:
+        line = []
+        for col in row:
+            if col['up'] or col['down'] or col['right'] or col['left']:
+                line.append('#')
+                result += 1
+            else:
+                line.append('.')
+        print(''.join(line))
+
+    print('Result: %s' % result)
+
+def main():
+    parser = argparse.ArgumentParser(description='Advent Of Code Solver %s' % __file__)
+    parser.add_argument('input_filename', help='Input', 
+                        nargs='?', default='input.txt')
+    args = vars(parser.parse_args())
+
+    with open(args['input_filename'],'r') as f:
+        solve(f.read())
+
+if __name__ == '__main__':
+    main()    
