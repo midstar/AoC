bc8942d1c484decace545858ce30a1fca8425e77
Joel Midstjärna <joel.midstjarna@gmail.com>
2023-12-19 23:37:44 +0100

    Puzzle 18 better comments

--- 2023/18/18B.py
+++ 2023/18/18B.py
@@ -31,7 +31,22 @@ def solve(input):
         area += 0.5 * (node_a[0] * node_b[1] - node_b[0] * node_a[1])
     area = abs(area)
 
-    # Area including border - sort of Pick's Theorem but +1 instead of -1 
+    # The problem with the area above is that includes only half of
+    # the border. Draw a rectancle with corners 0,0 0,3 3,3 and 3,0
+    # on a checkered paper you will se that the left and bottom 
+    # border are within the area and top and right border are outside 
+    # the area. Shoelace Formula will give area 9 but with the border
+    # (count squares on paper) the area is 9 + 7 = 16. 
+    #
+    # You kan use Pick's Theorem to calculate the inner are as:
+    #
+    # A = i + b/2 - 1  => i = A - b/2 + 1
+    #
+    # A is the area we calculated above (area variable)
+    #
+    # The area with border (AB) is AB = i + b
+    #
+    # Replace i and get AB = (A - b/2 + 1) + b = A + b/2 + 1
     result = int(area + steps_tot / 2 + 1)
 
     return result
