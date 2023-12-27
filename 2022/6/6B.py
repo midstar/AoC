2402fb1b72e86fab457c780fb56c719300744b56
Joel Midstjärna <joel.midstjarna@gmail.com>
2023-12-27 09:58:25 +0100

    Puzzle 6 solved

---
+++ 2022/6/6B.py
@@ -0,0 +1,21 @@
+import argparse, time
+
+def solve(input):
+    m = 14
+    for i in range(0, len(input) - m):
+        if len(set(input[i:i + m])) == m: return i + m
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='Advent Of Code Solver %s' % __file__)
+    parser.add_argument('input_filename', help='Input', 
+                        nargs='?', default='input.txt')
+    args = vars(parser.parse_args())
+
+    with open(args['input_filename'],'r') as f:
+        start_time = time.time()
+        result = solve(f.read())
+        print('Result: %s  (excution time %0.4f s)' % (result, time.time() - start_time))
+
+if __name__ == '__main__':
+    main()    
