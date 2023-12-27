85f3e0b5cb44deec3dd7443863de7108388ac10d
Joel Midstjärna <joel.midstjarna@gmail.com>
2023-12-27 09:37:33 +0100

    Puzzle 5 solved

---
+++ 2022/5/5B.py
@@ -0,0 +1,35 @@
+import argparse, time
+
+def solve(input):
+    stack_in, proc_in = tuple([[l for l in s_p.splitlines()] for s_p in input.split('\n\n')])
+
+    stacks = [[] for _ in stack_in[-1].split()]
+    for row in stack_in[:-1]:
+        for i, s in enumerate(stacks):
+            pos = 1 + i * 4
+            if row[pos] != ' ': s.insert(0, row[pos])
+
+    for proc in proc_in:
+        l, src, dst = tuple([int(v) for v in proc.split() if v.isnumeric()])
+        stacks[dst - 1].extend(stacks[src - 1][-1 * l:])
+        stacks[src - 1] = stacks[src - 1][:-1 * l]
+    
+    result = ''
+    for s in stacks:
+        result = result + s[-1]
+    return result
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='Advent Of Code Solver %s' % __file__)
+    parser.add_argument('input_filename', help='Input', 
+                        nargs='?', default='input.txt')
+    args = vars(parser.parse_args())
+
+    with open(args['input_filename'],'r') as f:
+        start_time = time.time()
+        result = solve(f.read())
+        print('Result: %s  (excution time %0.4f s)' % (result, time.time() - start_time))
+
+if __name__ == '__main__':
+    main()    
