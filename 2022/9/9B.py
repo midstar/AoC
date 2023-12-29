bd6931c89d136f428fb86e5380981cb03a7ae3e9
Joel Midstjärna <joel.midstjarna@gmail.com>
2023-12-29 13:54:44 +0100

    Puzzle 9 simplified more

--- 2022/9/9B.py
+++ 2022/9/9B.py
@@ -15,11 +15,8 @@ def follow(head, tail):
     dc = head[1] - tail[1]
     if abs(dr) <= 1 and abs(dc) <= 1:
         return tail # No move
-    dr_n, dc_n = (0,0)
-    if dc != 0:
-        dc_n = 1 if dc > 0 else -1
-    if dr != 0:
-        dr_n = 1 if dr > 0 else -1
+    dc_n = 1 if dc > 0 else -1 if dc < 0 else 0
+    dr_n = 1 if dr > 0 else -1 if dr < 0 else 0
 
     return (tail[0] + dr_n, tail[1] + dc_n)
 
