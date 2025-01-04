7c71bf6bc6ad1a20bbf4bf7d449d7292bda081eb
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-01-04 21:05:10 +0100

    2024 16 optimized

--- 2024/16A.py
+++ 2024/16A.py
@@ -11,10 +11,16 @@ DIR = {
 def get_new_pos(pos, dir):
     return (pos[0] + DIR[dir][0], pos[1] + DIR[dir][1])
 
+NEXT = {
+    'U' : [('U',1),('L',1001),('R',1001)],
+    'D' : [('D',1),('L',1001),('R',1001)],
+    'L' : [('L',1),('U',1001),('D',1001)],
+    'R' : [('R',1),('U',1001),('D',1001)]
+}
+
 def run(grid,start,end,dir):
     q = []
     heapq.heappush(q, (0, start, dir))
-
     visited = set()
     while q:
         points, pos, dir = heapq.heappop(q)
@@ -24,32 +30,19 @@ def run(grid,start,end,dir):
         if pos == end:
             return points
 
-        for dir2 in DIR.keys():
+        for dir2, points2 in NEXT[dir]:
             pos2 = get_new_pos(pos,dir2)
             if pos2 in grid:
-                points2 = points + 1
-                if dir != dir2:
-                    if dir in 'UD': 
-                        points2 += 1000 # 90 deg
-                        if dir2 in 'UD':
-                            continue
-                    elif dir in 'LR':
-                        points2 += 1000 # 90 deg
-                        if dir2 in 'LR':
-                            continue
-                heapq.heappush(q, (points2, pos2, dir2))
-
-    return 0
+                heapq.heappush(q, (points + points2, pos2, dir2))
 
 def solve(input):
-
-    grid = []
+    grid = set()
     start = None
     end = None
     for r, line in enumerate(input.splitlines()):
         for c, val in enumerate(line):
             if val != '#':
-                grid.append((r,c))
+                grid.add((r,c))
                 if val == 'S':
                     start = (r,c)
                 if val == 'E':
--- 2024/16B.py
+++ 2024/16B.py
@@ -11,10 +11,17 @@ DIR = {
 def get_new_pos(pos, dir):
     return (pos[0] + DIR[dir][0], pos[1] + DIR[dir][1])
 
+
+NEXT = {
+    'U' : [('U',1),('L',1001),('R',1001)],
+    'D' : [('D',1),('L',1001),('R',1001)],
+    'L' : [('L',1),('U',1001),('D',1001)],
+    'R' : [('R',1),('U',1001),('D',1001)]
+}
+
 def run(grid,start,end,dir):
     q = []
     heapq.heappush(q, (0, start, dir, {start}))
-
     visited = {}
     best_path = set()
     best_path_points = 10000000000000
@@ -30,33 +37,19 @@ def run(grid,start,end,dir):
             else:
                 return len(best_path)
 
-        for dir2 in DIR.keys():
+        for dir2, points2 in NEXT[dir]:
             pos2 = get_new_pos(pos,dir2)
             if pos2 in grid:
-                points2 = points + 1
-                if dir != dir2:
-                    if dir in 'UD': 
-                        points2 += 1000 # 90 deg
-                        if dir2 in 'UD':
-                            continue
-                    elif dir in 'LR':
-                        points2 += 1000 # 90 deg
-                        if dir2 in 'LR':
-                            continue
-                path2 = path | {pos2}
-                heapq.heappush(q, (points2, pos2, dir2, path2))
-
-    return 0
+                heapq.heappush(q, (points + points2, pos2, dir2, path | {pos2}))
 
 def solve(input):
-
-    grid = []
+    grid = set()
     start = None
     end = None
     for r, line in enumerate(input.splitlines()):
         for c, val in enumerate(line):
             if val != '#':
-                grid.append((r,c))
+                grid.add((r,c))
                 if val == 'S':
                     start = (r,c)
                 if val == 'E':
