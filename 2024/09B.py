c1944af5217186380b9a7bb4334269bb7ee488fc
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-01-06 18:39:44 +0100

    2024 9B optimized

--- 2024/09B.py
+++ 2024/09B.py
@@ -1,38 +1,50 @@
 import sys
 
+def get_file(fs, id, start_i):
+    for j in range(start_i, len(fs)):
+        id_, l_ = fs[j]
+        if id_ == id:
+            return j, l_
+
+def get_free_space(fs, min_size, max_i, start_i = 0):
+    first_free = -1
+    for j in range(start_i, len(fs)):
+        id_, l_ = fs[j]
+        if j > max_i:
+            return -1, -1, first_free
+        if id_ == -1:
+            if first_free == -1: 
+                first_free = j
+            if l_ >= min_size:
+                return j, l_, first_free
+    return -1, -1, first_free
+
 def solve(input):
     fs = []
+    files = {}
     id = 0
     for i, c in enumerate(input.strip()):
         l = int(c)
         val = -1
         if i % 2 == 0: # File
+            files[id] = (len(fs),l)
             val = id
             id += 1 
         if l > 0:
             fs.append((val,l))
 
+    first_free = 0
     for i in reversed(range(id)):
-        ni = -1
-        nl = 0
-        for j, (id_, l_) in enumerate(fs):
-            if id_ == i:
-                ni = j
-                nl = l_
-                break
+        ni, nl = get_file(fs, i, files[i][0])
+        free_i, l, first_free = get_free_space(fs,nl,ni, first_free)
+        if free_i >= 0:
+            fs[ni] = (-1, nl)
+            if l == nl:
+                fs[free_i] = (i, nl)
+            else:
+                fs[free_i] = (-1, l - nl)
+                fs.insert(free_i, (i, nl))
 
-        # Find free space
-        for j, (id_, l_) in enumerate(fs):
-            if j > ni:
-                break # No Free space
-            if id_ == -1 and l_ >= nl:
-                fs[ni] = (-1, nl)
-                if l_ == nl:
-                    fs[j] = (i, nl)
-                else:
-                    fs[j] = (-1, l_ - nl)
-                    fs.insert(j, (i, nl))
-                break
 
     checksum = 0
     multiplier = 0
