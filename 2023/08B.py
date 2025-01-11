bc58d85b623ec9917d274ec6ded30c62485090ed
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-01-11 21:18:05 +0100

    2023 8B optimized

--- 2023/08B.py
+++ 2023/08B.py
@@ -1,15 +1,25 @@
-import sys
-from functools import cmp_to_key
+import sys, math 
+
+def steps_to_z(instructions, nodes, node, xxZ_nodes):
+    steps = 0
+    i = 0
+    while True:
+        node = nodes[node][instructions[i]]
+        steps += 1
+        i += 1
+        if i >= len(instructions):
+            i = 0
+        if node in xxZ_nodes:
+            break
+    return steps
 
 def solve(input):
     lines = input.splitlines()
-    instr = lines.pop(0)
-    instructions = ""
-    for i in range (0, 1000):
-        instructions += instr
+    instructions = lines.pop(0)
     lines.pop(0)
     nodes = {}
-    current_nodes = []
+    xxA_nodes = set()
+    xxZ_nodes = set()
     for line in lines:
         name_conn = line.split('=')
         name = name_conn[0].strip()
@@ -21,50 +31,12 @@ def solve(input):
             'R' : name_right
         }
         if name[2] == 'A':
-            current_nodes.append(name)
+            xxA_nodes.add(name)
+        if name[2] == 'Z':
+            xxZ_nodes.add(name)
 
-    # Preprocess
-    for name, node in nodes.items():
-        node['endNodes'] = []
-        name2 = name
-        for i in range(0, len(instructions)):
-            name2 = nodes[name2][instructions[i]]
-            if name2[2] == 'Z':
-                node['endNodes'].append(i + 1)
-        node['nextNode'] = name2
-    
-    #print(current_nodes)
-
-    # Check
-    step_big = len(instructions)
-    steps = 0
-    while True:
-        endNodes = nodes[current_nodes[0]]['endNodes']
-        found = -1
-        maxMatches = 0
-        for endNode in endNodes:
-            found = endNode
-            for i in range(1, len(current_nodes)):
-                if found not in nodes[current_nodes[i]]['endNodes']:
-                    found = -1
-                    break
-                if maxMatches < i:
-                    maxMatches = i
-            if found != -1:
-                break
-        if found != -1:
-            steps += found
-            break # We are done
-        
-        for i in range(0, len(current_nodes)):
-            current_nodes[i] = nodes[current_nodes[i]]['nextNode']
-
-        steps += step_big
-
-        #if (maxMatches > 4):
-        #    print('%s %s' % (current_nodes, steps))
-
-    return steps
+    steps = [steps_to_z(instructions, nodes, node, xxZ_nodes) for node in xxA_nodes]
+    return math.lcm(*steps)
 
 if __name__ == '__main__':
         print(solve(open(sys.argv[1]).read()))
