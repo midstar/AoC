a5f2836a28beb6976b115a26e690f8071e9aead5
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-01-19 08:47:17 +0100

    2023 22A optimized

--- 2023/22A.py
+++ 2023/22A.py
@@ -1,122 +1,70 @@
 import sys
+
 from functools import cmp_to_key
 
-def compare(line1, line2):
-    z_min = [0, 0]
-    z_max = [0, 0]
-    for i, line in enumerate([line1, line2]):
-        A_B = line.split('~')
-        A_Z = [int(c) for c in A_B[0].split(',')][2]
-        B_Z = [int(c) for c in A_B[1].split(',')][2]
-        z_min[i] = min(A_Z, B_Z)
-        z_max[i] = max(A_Z, B_Z)
-    if z_min[0] != z_min[1]:
-        return z_min[0] - z_min[1]
-    return z_max[0] - z_max[1]
+def compare_bricks(brick1, brick2):
+    z_min = min(b[2] for b in brick1) -  min(b[2] for b in brick2)
+    if z_min != 0: return z_min
+    return max(b[2] for b in brick1) -  max(b[2] for b in brick2)
 
-def remove_brick(space, bricks, index):
-    for coord in bricks[index]:
-        if coord not in space:
-            print(f'Error! Brick {index} coord {coord} free')
-            exit(1)
-        del space[coord]
+def place(blocks,brick):
+    min_z = min(b[2] for b in brick)
+    while min_z > 1:
+        brick2 = {(b[0], b[1], b[2] - 1) for b in brick}
+        if len(blocks & brick2) != 0:
+             break
+        brick = brick2
+        min_z -= 1
+    return brick
 
-def add_brick(space, bricks, index):
-    for coord in bricks[index]:
-        if coord in space:
-            print(f'Error! Brick {index} coord {coord} not free')
-            exit(1)
-        space[coord] = index
+def brick_steady(blocks,brick):
+    blocks -= brick 
+    result = True
+    if min(b[2] for b in brick) > 1:
+        brick2 = {(b[0], b[1], b[2] - 1) for b in brick}
+        result = len(brick2 & blocks) > 0
+    blocks |= brick
+    return result   
 
-def free_z_below(space, bricks, index):
-    brick = bricks[index]
-    z_down = brick[0][2]
-    free_z = 10000000
-    for (_, _, z) in brick:
-        z_down = z if z < z_down else z_down
-    for (x, y, z) in brick:
-        if z != z_down: continue
-        z_offset = 0
-        while((z - z_offset) > 1):
-            z_offset += 1
-            if (x, y, z - z_offset) in space:
-                z_offset -= 1
+def safe_to_remove(blocks, bricks, i):
+    brick = bricks[i]
+    result = True
+    blocks -= brick
+
+    support_blocks = {(b[0], b[1], b[2] + 1) for b in brick}
+    support_blocks &= blocks
+    while len(support_blocks) > 0 and i < (len(bricks) - 1):
+        i += 1
+        brick2 = bricks[i]
+        match_blocks = support_blocks & brick2
+        if len(match_blocks) > 0:
+            if not brick_steady(blocks,brick2):
+                result = False
                 break
-        free_z = min(z_offset, free_z)
-        
-    return free_z
+            support_blocks -= match_blocks
 
+    blocks |= brick
+    return result
 
 def solve(input):
-    lines = input.splitlines()
+    unsorted_bricks = []
+    for line in input.splitlines():
+        x, y, z, x2, y2, z2 = map(int,line.replace('~',',').split(','))
+        brick = {(x,y,z)}
+        while x != x2 or y != y2 or z != z2:
+            if x < x2: x += 1
+            if y < y2: y += 1
+            if z < z2: z += 1
+            brick.add((x,y,z))
+        unsorted_bricks.append(brick) 
 
-    # Sort based on z
-    compare_key = cmp_to_key(compare)
-    lines = sorted(lines, key=compare_key)
-
-    # Create bricks
     bricks = []
-    for i, line in enumerate(lines):
-        A_B = line.split('~')
-        A = [int(c) for c in A_B[0].split(',')]
-        B = [int(c) for c in A_B[1].split(',')]
+    blocks = set()
+    for brick in sorted(unsorted_bricks,key=cmp_to_key(compare_bricks)):
+        bricks.append(place(blocks,brick))
+        blocks |= bricks[-1]
 
-        base = A
-        size = 1
-        axis = 0
-        xyz = [0,1,2]
-        for i in xyz:
-            diff = A[i] - B[i]
-            if diff != 0:
-                axis = i
-                if diff > 0:
-                    base = B
-                size = abs(diff) + 1
-                break
-
-        coords = []
-        for v in range(base[axis], base[axis] + size):
-            base[axis] = v
-            coords.append((base[0], base[1], base[2]))
-        if len(coords) == 0:
-            print('Error', line, A, B, base, size)
-            exit()
-        bricks.append(coords)
-
-    # Add bricks to space
-    space = {}
-    for i in range(len(bricks)):
-        add_brick(space, bricks, i)
-
-    # Drop bricks one-by-one
-    moved = True
-    while(moved):
-        moved = False
-        for i in range(len(bricks)):
-            z_offset = free_z_below(space, bricks, i)
-            if z_offset > 0:
-                moved = True
-                remove_brick(space, bricks, i)
-                new_coords = []
-                for (x, y, z) in bricks[i]:
-                    new_coords.append((x, y, z - z_offset))
-                bricks[i] = new_coords
-                add_brick(space, bricks, i)    
-
-    # Test remove one-by-one
-    result = 0
-    for i in range(len(bricks)):
-        movable = True
-        remove_brick(space, bricks, i)
-        for j in range(len(bricks)):
-            if j != i:
-                if free_z_below(space, bricks, j) != 0:
-                    movable = False   
-                    break   
-        add_brick(space, bricks, i) 
-        if movable: result += 1
-
-    return result
+    return sum(safe_to_remove(blocks,bricks,i) for i in range(len(bricks)))
 
 if __name__ == '__main__':
         print(solve(open(sys.argv[1]).read()))
