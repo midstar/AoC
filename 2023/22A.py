177f48e6a4d1ac757e2c7dbe9836540ac18c4e45
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-01-19 13:10:31 +0100

    2023 22 optimized

--- 2023/22A.py
+++ 2023/22A.py
@@ -7,47 +7,31 @@ def compare_bricks(brick1, brick2):
     if z_min != 0: return z_min
     return max(b[2] for b in brick1) -  max(b[2] for b in brick2)
 
-def place(blocks,brick):
+def place(blocks,brick,i):
+    rests_on = set()
     min_z = min(b[2] for b in brick)
     while min_z > 1:
         brick2 = {(b[0], b[1], b[2] - 1) for b in brick}
-        if len(blocks & brick2) != 0:
+        for block in brick2:
+            if block in blocks:
+                rests_on.add(blocks[block])
+        if len(rests_on) != 0:
              break
         brick = brick2
         min_z -= 1
-    return brick
+    for block in brick: blocks[block] = i
+    return rests_on
 
-def brick_steady(blocks,brick):
-    blocks -= brick 
-    result = True
-    if min(b[2] for b in brick) > 1:
-        brick2 = {(b[0], b[1], b[2] - 1) for b in brick}
-        result = len(brick2 & blocks) > 0
-    blocks |= brick
-    return result   
-
-def safe_to_remove(blocks, bricks, i):
-    brick = bricks[i]
-    result = True
-    blocks -= brick
-
-    support_blocks = {(b[0], b[1], b[2] + 1) for b in brick}
-    support_blocks &= blocks
-    while len(support_blocks) > 0 and i < (len(bricks) - 1):
-        i += 1
-        brick2 = bricks[i]
-        match_blocks = support_blocks & brick2
-        if len(match_blocks) > 0:
-            if not brick_steady(blocks,brick2):
-                result = False
-                break
-            support_blocks -= match_blocks
-
-    blocks |= brick
-    return result
+def removable(bricks_depends, i):
+    fallen = {i}
+    for j in range(i+1,len(bricks_depends)):
+        dep = bricks_depends[j]
+        if dep and dep.issubset(fallen):
+            return False
+    return True
 
 def solve(input):
-    unsorted_bricks = []
+    bricks = []
     for line in input.splitlines():
         x, y, z, x2, y2, z2 = map(int,line.replace('~',',').split(','))
         brick = {(x,y,z)}
@@ -56,15 +40,14 @@ def solve(input):
             if y < y2: y += 1
             if z < z2: z += 1
             brick.add((x,y,z))
-        unsorted_bricks.append(brick) 
+        bricks.append(brick) 
 
-    bricks = []
-    blocks = set()
-    for brick in sorted(unsorted_bricks,key=cmp_to_key(compare_bricks)):
-        bricks.append(place(blocks,brick))
-        blocks |= bricks[-1]
+    bricks_depends = []
+    blocks = {}
+    for i, brick in enumerate(sorted(bricks,key=cmp_to_key(compare_bricks))):
+        bricks_depends.append(place(blocks,brick,i))
 
-    return sum(safe_to_remove(blocks,bricks,i) for i in range(len(bricks)))
+    return sum(removable(bricks_depends,i) for i in range(len(bricks)))
 
 if __name__ == '__main__':
         print(solve(open(sys.argv[1]).read()))
--- 2023/22B.py
+++ 2023/22B.py
@@ -7,17 +7,20 @@ def compare_bricks(brick1, brick2):
     if z_min != 0: return z_min
     return max(b[2] for b in brick1) -  max(b[2] for b in brick2)
 
-def place(blocks,brick):
+def place(blocks,brick,i):
     rests_on = set()
     min_z = min(b[2] for b in brick)
     while min_z > 1:
         brick2 = {(b[0], b[1], b[2] - 1) for b in brick}
-        rests_on = blocks & brick2
+        for block in brick2:
+            if block in blocks:
+                rests_on.add(blocks[block])
         if len(rests_on) != 0:
              break
         brick = brick2
         min_z -= 1
-    return brick, rests_on
+    for block in brick: blocks[block] = i
+    return rests_on
 
 def will_fall(bricks_depends, i):
     fallen = {i}
@@ -28,7 +31,7 @@ def will_fall(bricks_depends, i):
     return len(fallen) - 1
 
 def solve(input):
-    unsorted_bricks = []
+    bricks = []
     for line in input.splitlines():
         x, y, z, x2, y2, z2 = map(int,line.replace('~',',').split(','))
         brick = {(x,y,z)}
@@ -37,24 +40,12 @@ def solve(input):
             if y < y2: y += 1
             if z < z2: z += 1
             brick.add((x,y,z))
-        unsorted_bricks.append(brick) 
+        bricks.append(brick) 
 
-    bricks = []
     bricks_depends = []
-    blocks = set()
-    for brick in sorted(unsorted_bricks,key=cmp_to_key(compare_bricks)):
-        placed_brick, rests_on = place(blocks,brick)
-        depends = set()
-        bricks_depends.append(depends)
-        i = 0
-        while rests_on and i < len(bricks):
-            match_block = rests_on & bricks[i]
-            if match_block:
-                depends.add(i)
-                rests_on -= match_block
-            i += 1
-        bricks.append(placed_brick)
-        blocks |= bricks[-1]
+    blocks = {}
+    for i, brick in enumerate(sorted(bricks,key=cmp_to_key(compare_bricks))):
+        bricks_depends.append(place(blocks,brick,i))
 
     return sum(will_fall(bricks_depends,i) for i in range(len(bricks)))
 
