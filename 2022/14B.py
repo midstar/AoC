26f61b6ac1c4b8fc4845ede451782fe2274d3460
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-01-25 07:40:50 +0100

    2022 14 solved

--- 2022/14B.py
+++ 2022/14B.py
@@ -2,15 +2,14 @@ import sys
 
 def sand_fall(grid, pos, max_y):
     x, y = pos
-    if y >= max_y:
-        return None
+    if y == max_y:
+        return pos
     elif (x, y + 1) not in grid:
         return sand_fall(grid, (x, y + 1), max_y)
     elif (x - 1, y + 1) not in grid:
         return sand_fall(grid, (x - 1, y + 1), max_y)
     elif (x + 1, y + 1) not in grid:
         return sand_fall(grid, (x + 1, y + 1), max_y)
-    grid.add(pos)
     return pos
 
 
@@ -28,12 +27,16 @@ def solve(input):
                     x0 = x0 - 1 if x < x0 else x0 + 1 if x > x0 else x0
                     y0 = y0 - 1 if y < y0 else y0 + 1 if y > y0 else y0
                     grid.add((x0,y0))
-                    
-    max_y = max(y for _, y in grid)
-
+    max_y = max(y for _, y in grid) + 1
     result = 0
-    while sand_fall(grid, (500, 0), max_y) != None:
-        result += 1
+    while True:
+        pos = sand_fall(grid, (500, 0), max_y)
+        if pos != (500, 0):
+            grid.add(pos)
+            result += 1
+        else:
+            result += 1 # Count in (500, 0)
+            break
     return result
         
 if __name__ == '__main__':
