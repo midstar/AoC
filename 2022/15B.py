6a17db16bd091ebb0b349a80f0c7ca1735a041b1
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-02-08 08:47:00 +0100

    2022 15B not working

--- 2022/15B.py
+++ 2022/15B.py
@@ -9,7 +9,7 @@ def sides(sensor):
     bottom  = y + d
     right   = x + d
     left    = x - d
-    return [((top,x),(right,y)),((top,x),(left,y)),((bottom,x),(right,y)),((bottom,x),(left,y))]
+    return [((x,top),(right,y)),((x,top),(left,y)),((x,bottom),(right,y)),((x,bottom),(left,y))]
 
 # See https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
 def intersection(line1,line2):
@@ -24,22 +24,34 @@ def intersection(line1,line2):
     Py = y1+t*(y2-y1)
     return round(Px),round(Py)
 
+def beacon_possible(sensors,x,y):
+    for sx,sy,d in sensors:
+        if dist(x,y,sx,sy) <= d:
+            return False
+    return True
+
 def solve(input):
     sensors = []
     for line in input.splitlines():
         x,y,bx,by = list(map(int,re.findall(r'=(-?\d+)', line)))
         sensors.append((x,y,dist(x,y,bx,by)))
 
+    MAX = 4000000 # For example use 20
     for i, sensor in enumerate(sensors[:-1]):
         for side in sides(sensor):
             for sensor2 in sensors[i+1:]:
                 for side2 in sides(sensor2):
                     inters = intersection(side,side2)
-                    if inters:
-                        print(inters)
+                    if not inters: continue
+                    ix, iy = inters
+                    for dx, dy in [(0,0),(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]:
+                        x = ix + dx
+                        y = iy + dy
+                        if x < 0 or x > MAX or y < 0 or y > MAX: continue
+                        if beacon_possible(sensors,x,y):
+                            return x * 4000000 + y
 
-
-    return 0
+    return None
         
 if __name__ == '__main__':
     print(solve(open(sys.argv[1]).read()))
