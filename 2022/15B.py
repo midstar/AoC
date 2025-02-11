7b26a957bba3b155408f2f15c7d95c0d669fff28
Joel Midstjärna <joel.midstjarna@gmail.com>
2025-02-11 20:46:15 +0100

    2022 15B solved

--- 2022/15B.py
+++ 2022/15B.py
@@ -1,5 +1,18 @@
 import sys, re
 
+# See https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+# Wiki is invalid got some help from ChatGPT
+def intersection(line1,line2):
+    (x1, y1), (x2, y2) = line1
+    (x3, y3), (x4, y4) = line2
+    den = (x2 - x1) * (y4 - y3) - (y2 - y1) * (x4 - x3)
+    if den == 0: return None
+    t = ((x3 - x1) * (y4 - y3) - (y3 - y1) * (x4 - x3)) / den
+    u = ((x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)) / den
+    if 0 <= t <= 1 and 0 <= u <= 1:
+        return x1+t*(x2-x1), y1+t*(y2-y1)
+    return None
+
 def dist(x1,y1,x2,y2):
     return abs(x1-x2) + abs(y1-y2)
 
@@ -11,18 +24,18 @@ def sides(sensor):
     left    = x - d
     return [((x,top),(right,y)),((x,top),(left,y)),((x,bottom),(right,y)),((x,bottom),(left,y))]
 
-# See https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
-def intersection(line1,line2):
-    (x1, y1), (x2, y2) = line1
-    (x3, y3), (x4, y4) = line2
-    den = (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)
-    if den == 0: return None
-    t = ((x1-x3)*(y3-y4)-(y1-y3)*(x3-x4)) / den
-    u = ((x1-x2)*(y1-y3)-(y1-y2)*(x1-x3)) / den
-    if t < 0 or t > 1 or u < 0 or u > 1: return None
-    Px = x1+t*(x2-x1)
-    Py = y1+t*(y2-y1)
-    return round(Px),round(Py)
+#MAX = 20      # Example
+MAX = 4000000 
+def is_possible_around_point(sensors, px,py):
+    for dx, dy in [(-1,-1),(0,-1),(1,-1), \
+                   (-1, 0),(0, 0),(1, 0), \
+                   (-1, 1),(0, 1),(1, 1)]:
+        x = px + dx
+        y = py + dy 
+        if x < 0 or x > MAX or y < 0 or y > MAX: continue
+        if beacon_possible(sensors,x,y):
+            return int(x * 4000000 + y)  
+    return None 
 
 def beacon_possible(sensors,x,y):
     for sx,sy,d in sensors:
@@ -36,21 +49,14 @@ def solve(input):
         x,y,bx,by = list(map(int,re.findall(r'=(-?\d+)', line)))
         sensors.append((x,y,dist(x,y,bx,by)))
 
-    MAX = 4000000 # For example use 20
     for i, sensor in enumerate(sensors[:-1]):
         for side in sides(sensor):
             for sensor2 in sensors[i+1:]:
                 for side2 in sides(sensor2):
                     inters = intersection(side,side2)
                     if not inters: continue
-                    ix, iy = inters
-                    for dx, dy in [(0,0),(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]:
-                        x = ix + dx
-                        y = iy + dy
-                        if x < 0 or x > MAX or y < 0 or y > MAX: continue
-                        if beacon_possible(sensors,x,y):
-                            return x * 4000000 + y
-
+                    v = is_possible_around_point(sensors,*inters)
+                    if v: return v
     return None
         
 if __name__ == '__main__':
